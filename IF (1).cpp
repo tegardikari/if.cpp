a45c9ba6ca49229c64cbcf822dde259cf7154f77
tegardikari <111736182+tegardikari@users.noreply.github.com>
2022-11-28 14:16:42 +0700

    Add files via upload

---
+++ IF (1).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+  int main (){
+    int nilai =78;
+    if (nilai > 78){
+      cout<<"Lulus"<<endl;
+}
+else{
+cout<<"Tidak lulus"<<endl;
+}
+  return 0;
+}      
+ 
+     
+     
+       
+     
+         
+         
+        
+    
+
+    
---
+++ if.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int nilai ;
+    cout << "Nilai kamu = ";
+    cin >> nilai;
+    if (nilai > 89){
+      cout << "Lulus";
+      }else if ( nilai == 89){
+        cout << "Lulus Kkm";
+        }else{
+          cout << "Tidak Lulus";
+        }
+          return 0;
+ }      
